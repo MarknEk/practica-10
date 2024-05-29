6a6e8418ac3b5e8a79d00805a6869c90f87991ba
admin <admin@USER-PC>
2024-05-29 03:42:42 +0300

    Добавьте файлы проекта.

---
+++ practica 10/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace practica_10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBoxProgrammer = new System.Windows.Forms.ListBox();
+            this.btnDeleteFirst1 = new System.Windows.Forms.Button();
+            this.btnMutate2 = new System.Windows.Forms.Button();
+            this.btnDeleteLast1 = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBoxProgrammer
+            // 
+            this.listBoxProgrammer.FormattingEnabled = true;
+            this.listBoxProgrammer.ItemHeight = 16;
+            this.listBoxProgrammer.Location = new System.Drawing.Point(14, 24);
+            this.listBoxProgrammer.Name = "listBoxProgrammer";
+            this.listBoxProgrammer.Size = new System.Drawing.Size(447, 228);
+            this.listBoxProgrammer.TabIndex = 0;
+            // 
+            // btnDeleteFirst1
+            // 
+            this.btnDeleteFirst1.Location = new System.Drawing.Point(14, 258);
+            this.btnDeleteFirst1.Name = "btnDeleteFirst1";
+            this.btnDeleteFirst1.Size = new System.Drawing.Size(143, 50);
+            this.btnDeleteFirst1.TabIndex = 2;
+            this.btnDeleteFirst1.Text = "Удалить";
+            this.btnDeleteFirst1.UseVisualStyleBackColor = true;
+            this.btnDeleteFirst1.Click += new System.EventHandler(this.btnDeleteFirst_Click);
+            // 
+            // btnMutate2
+            // 
+            this.btnMutate2.Location = new System.Drawing.Point(320, 258);
+            this.btnMutate2.Name = "btnMutate2";
+            this.btnMutate2.Size = new System.Drawing.Size(141, 50);
+            this.btnMutate2.TabIndex = 3;
+            this.btnMutate2.Text = "Мульти";
+            this.btnMutate2.UseVisualStyleBackColor = true;
+            this.btnMutate2.Click += new System.EventHandler(this.btnMutate_Click);
+            // 
+            // btnDeleteLast1
+            // 
+            this.btnDeleteLast1.Location = new System.Drawing.Point(163, 258);
+            this.btnDeleteLast1.Name = "btnDeleteLast1";
+            this.btnDeleteLast1.Size = new System.Drawing.Size(151, 50);
+            this.btnDeleteLast1.TabIndex = 4;
+            this.btnDeleteLast1.Text = "Удалить последнюю запись";
+            this.btnDeleteLast1.UseVisualStyleBackColor = true;
+            this.btnDeleteLast1.Click += new System.EventHandler(this.btnDeleteLast_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(14, 314);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(447, 52);
+            this.btnReset.TabIndex = 5;
+            this.btnReset.Text = "Сброс";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(476, 377);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnDeleteLast1);
+            this.Controls.Add(this.btnMutate2);
+            this.Controls.Add(this.btnDeleteFirst1);
+            this.Controls.Add(this.listBoxProgrammer);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBoxProgrammer;
+        private System.Windows.Forms.Button btnDeleteFirst1;
+        private System.Windows.Forms.Button btnMutate2;
+        private System.Windows.Forms.Button btnDeleteLast1;
+        private System.Windows.Forms.Button btnReset;
+    }
+}
+
---
+++ practica 10/Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace practica_10
+{
+    public partial class Form1 : Form
+    {
+        private Programmer programmer;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Ваш код инициализации формы, если это необходимо
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeProgrammer();
+            FillLists();
+        }
+        private void FillLists()
+        {
+            // Добавление элементов в список программиста
+            programmer.Add("Code 1");
+            programmer.Add("Code 2");
+
+            // Обновление состояния списков на форме
+            UpdateLists();
+        }
+
+        // Обновление состояния списков на форме
+        private void UpdateLists()
+        {
+            listBoxProgrammer.Items.Clear();
+
+            // Получение текущего состояния списка программиста
+            List<string> programmerList = programmer.GetList();
+
+            // Отображение элементов в списке программиста на форме
+            foreach (string item in programmerList)
+            {
+                listBoxProgrammer.Items.Add(item);
+            }
+        }
+        private void InitializeProgrammer()
+        {
+            // Создание объекта программиста
+            programmer = new Programmer();
+
+            // Подписка на события
+            programmer.Delete += Programmer_Delete;
+            programmer.Mutate += Programmer_Mutate;
+
+            // Добавление элементов в список программиста
+            programmer.Add("Code 1");
+            programmer.Add("Code 2");
+        }
+
+        // Обработчик события удаления
+        private void Programmer_Delete(object sender, EventArgs e)
+        {
+            MessageBox.Show("Программист удалил элемент.");
+            UpdateLists(); // Обновление состояния списка программиста на форме
+        }
+
+        // Обработчик события мутации
+        private void Programmer_Mutate(object sender, EventArgs e)
+        {
+            MessageBox.Show("Программист изменил список.");
+            UpdateLists(); // Обновление состояния списка программиста на форме
+        }
+
+        // Обработчик кнопки "Удалить первый элемент"
+        private void btnDeleteFirst_Click(object sender, EventArgs e)
+        {
+            programmer.DeleteFirst();
+        }
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            programmer.Reset(); // Вызываем метод сброса состояния списка программиста
+            UpdateLists(); // Обновляем отображение списка на форме
+        }
+        // Обработчик кнопки "Удалить последний элемент"
+        private void btnDeleteLast_Click(object sender, EventArgs e)
+        {
+            programmer.DeleteLast();
+        }
+
+        // Обработчик кнопки "Мутация списка"
+        private void btnMutate_Click(object sender, EventArgs e)
+        {
+            programmer.MutateList();
+        }
+    }
+
+    // Класс программиста
+    public class Programmer
+    {
+
+        // Событие удаления
+        public event EventHandler Delete;
+
+        // Событие мутации
+        public event EventHandler Mutate;
+
+        private List<string> list;
+
+        public Programmer()
+        {
+            list = new List<string>();
+        }
+        // Метод для сброса состояния списка программиста к исходному
+        public void Reset()
+        {
+            list.Clear(); // Очищаем список программиста
+            Add("Code 1"); // Добавляем исходные элементы
+            Add("Code 2");
+        }
+
+        // Добавление элемента в список
+        public void Add(string item)
+        {
+            list.Add(item);
+        }
+
+        // Удаление первого элемента
+        public void DeleteFirst()
+        {
+            if (list.Count > 0)
+            {
+                list.RemoveAt(0);
+                OnDelete(EventArgs.Empty);
+            }
+        }
+
+        // Удаление последнего элемента
+        public void DeleteLast()
+        {
+            if (list.Count > 0)
+            {
+                list.RemoveAt(list.Count - 1);
+                OnDelete(EventArgs.Empty);
+            }
+        }
+
+        // Мутация списка
+        public void MutateList()
+        {
+            // Реализация мутации может быть любой
+            // Например, перемешивание элементов
+            list.Reverse();
+            OnMutate(EventArgs.Empty);
+        }
+
+        protected virtual void OnDelete(EventArgs e)
+        {
+            Delete?.Invoke(this, e);
+        }
+
+        protected virtual void OnMutate(EventArgs e)
+        {
+            Mutate?.Invoke(this, e);
+        }
+
+        // Получение текущего состояния списка
+        public List<string> GetList()
+        {
+            return list;
+        }
+    }
+}
